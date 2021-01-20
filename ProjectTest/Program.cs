71dc374ddf15b632c1b33dd277f1cb6c0783ff90
SevostyanovAndrey <aannddrreejj19@gmail.com>
2021-01-20 13:29:01 +0300

    Начальный возврат модуля ProjectTest

---
+++ ProjectTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProjectTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите в");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Большим числом является: "+a);
+            
+            }
+            else {
+                if(a == b)
+                {
+                    Console.WriteLine("Эти числа равны");
+                        }
+                else
+                {
+                    Console.WriteLine("Большим числом является: " + b);
+                }
+                
+            }
+            
+        
+            Console.ReadKey();
+        }
+    }
+}
